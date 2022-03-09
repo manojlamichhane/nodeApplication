f1f18962089bf413745499b428c35235df246d6f
manojlamichhane <lammichhanemanoj1@gmail.com>
2022-03-08 23:19:32 -0500

    few changes for heroku

--- server/index.js
+++ server/index.js
@@ -1,39 +1,37 @@
-
 const express = require("express");
-const {Pool ,Client} = require("pg")
+const { Pool, Client } = require("pg");
 const pool = new Pool({
   user: "ece",
   host: "database-1.ciivyj7ykn79.us-east-1.rds.amazonaws.com",
   database: "Medicine_list",
   password: "Inspiron11",
-  port: 5432
+  port: 5432,
 });
 
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 const app = express();
 var medicines = [];
 
-pool.connect((err,client,done)=>{
-if(err) throw err
-client.query("SELECT * FROM medicines ",(err,res)=>{
-  done()
-  if(err){
-    console.log("err",err)
-  }else{
-    res.rows.map((row)=>{
-      console.log("data",row)
-      medicines.push(row)
-    })
-  }
-})
-})
-
-
-app.get("/api", (req, res) => {
-    res.json({ 
-      data:medicines
-   });
+pool.connect((err, client, done) => {
+  if (err) throw err;
+  client.query("SELECT * FROM medicines ", (err, res) => {
+    done();
+    if (err) {
+      console.log("err", err);
+    } else {
+      res.rows.map((row) => {
+        console.log("data", row);
+        medicines.push(row);
+      });
+    }
   });
+});
+
+app.get("/medicines", (req, res) => {
+  res.json({
+    data: medicines,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
